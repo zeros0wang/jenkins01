ad1365e364264d2bc91e39f936ddb1d57716b73b
wangpeng <wangpeng@hongkang-life.com>
2020-11-01 11:42:50 +0800

    新疆项目

---
+++ src/main/java/com/wp/platform/TestController.java
@@ -0,0 +1,19 @@
+package com.wp.platform;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author wp
+ * @Description
+ * @Date 2020/11/1 11:20
+ */
+@RestController
+@RequestMapping("/jenkins")
+public class TestController {
+    @GetMapping("/test")
+    public Object test() {
+        return "hello world man!";
+    }
+}
