6c2f1580b45be34e4839d17d987220dea7c3e17d
wangpeng <wangpeng@hongkang-life.com>
2020-11-02 12:23:26 +0800

    新疆修改001

---
+++ src/main/java/com/wp/platform/Jenkins01Application.java
@@ -0,0 +1,13 @@
+package com.wp.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jenkins01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jenkins01Application.class, args);
+    }
+
+}
